eef9cd23db185285a532d7425b404a0fef15d627
viljami_irri <viljami.irri@tuni.fi>
2023-11-14 14:25:34 +0200

    failing test case

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -3,3 +3,7 @@ import capitalize from '../src/capitalize';
 test('Capitalize uppercase word', () => {
     expect(capitalize('FRED')).toBe('Fred');
 });
+
+test('Failing test', () => {
+    expect(capitalize('FRED')).toBe('FRed');
+});
