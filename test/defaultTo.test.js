95a56900209fed01d119d60bd0ca7cfcd218b3fa
viljami_irri <viljami.irri@tuni.fi>
2023-11-14 18:08:07 +0200

    defaultTo unit tests

---
+++ test/defaultTo.test.js
@@ -0,0 +1,31 @@
+import defaultTo from '../src/defaultTo.js';
+
+describe('defaultTo', function() {
+    test('should return defaultValue if value is NaN', () => {
+        expect(defaultTo(NaN,1)).toEqual(1);
+    });
+
+    test('should return defaultValue if value is null', () => {
+        expect(defaultTo(null,1)).toEqual(1);
+    });
+
+    test('should return defaultValue if value is undefined', () => {
+        expect(defaultTo(undefined,1)).toEqual(1);
+    });
+
+    test('should return value as number', () => {
+        expect(defaultTo(2,1)).toEqual(2);
+    });
+
+    test('should return value as string', () => {
+        expect(defaultTo('123',1)).toEqual('123');
+    });
+
+    test('should return value as boolean', () => {
+        expect(defaultTo(false,1)).toEqual(false);
+    });
+
+    test('should return boolean value as not a number', () => { // New test case (Redundant?)
+        expect(defaultTo(true,0)).not.toEqual(Number(true));
+    });
+});
