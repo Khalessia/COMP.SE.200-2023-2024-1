e48c87b4fde3dfd25ca51595c72b6fcb4379fa4a
viljami_irri <viljami.irri@tuni.fi>
2023-11-14 16:10:59 +0200

    Capitalize unit tests

--- test/capitalize.test.js
+++ test/capitalize.test.js
@@ -1,9 +1,36 @@
 import capitalize from '../src/capitalize';
 
-test('Capitalize uppercase word', () => {
-    expect(capitalize('FRED')).toBe('Fred');
-});
+describe('Capitalize', () => {
+    test('Capitalize empty string', () => {
+        expect(capitalize('')).toBe('');
+    });
 
-test('Failing test', () => {
-    expect(capitalize('FRED')).toBe('FRed');
+    test('Capitalize null', () => {
+        expect(capitalize(null)).toBe('');
+    });
+
+    test('Capitalize undefined', () => {
+        expect(capitalize(undefined)).toBe('');
+    });
+
+    test('Capitalize capitalized word', () => {
+        expect(capitalize('Abc')).toBe('Abc');
+    });
+
+    test('Capitalize uppercase word with numbers', () => {
+        expect(capitalize('A012ABC')).toBe('A012abc');
+    });
+
+    test('Capitalize uncapitalized word with numbers', () => {
+        expect(capitalize('a012ABC')).toBe('A012abc');
+    });
+
+    test('Capitalize word starting with numbers', () => {
+        expect(capitalize('012aBC')).toBe('012abc');
+    });
+
+    test('Capitalize word starting with a negative number', () => {
+        expect(capitalize('-012aBC')).toBe('-012abc');
+    });
+
 });
