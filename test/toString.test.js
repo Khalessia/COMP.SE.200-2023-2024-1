4c0d98b4ded09b473bbac8337e8670d32d551890
Samantha Bavautdin <samantha.bavautdin@tuni.fi>
2023-11-25 16:10:36 +0200

    Unit tests for toString.js updated by Test Plan

--- test/toString.test.js
+++ test/toString.test.js
@@ -6,14 +6,56 @@ describe('toString', () => {
         expect(result).toBe('');
     });
 
+    it('should convert undefined to an empty string', () => {
+        const result = toString(undefined);
+        expect(result).toBe('');
+    });
+
+    it('should convert a number to a string', () => {
+        const result = toString(1);
+        expect(result).toBe('1');
+    });
+
+    it('should convert a float number to a string', () => {
+        const result = toString(1.0);
+        expect(result).toBe('1.0');
+    });
+
     it('should preserve the sign of -0', () => {
         const result = toString(-0);
         expect(result).toBe('-0');
     });
 
+    it('should return the same string', () => {
+        const result = toString("string");
+        expect(result).toBe("string");
+    });
+
+    // Redundant?
+    it('should return the same string when input is a string of numbers "123"', () => {
+        const result = toString("123");
+        expect(result).toBe("123");
+    });
+
+    // Redundant?
+    it('should return the same string when input is a string of numbers and characters "123abc"', () => {
+        const result = toString("123abc");
+        expect(result).toBe("123abc");
+    });
+
     it('should convert an array to a string', () => {
         const result = toString([1, 2, 3]);
         expect(result).toBe('1,2,3');
+    });
+
+    it('should convert an array with null to a string', () => {
+        const result = toString([1, null, 3]);
+        expect(result).toBe('1,,3');
+    });
+
+    it('should convert an array with numbers and a string to a string', () => {
+        const result = toString([1, "abc", 3]);
+        expect(result).toBe('1,abc,3');
     });
 
     it('should convert a symbol to a string', () => {
@@ -22,8 +64,5 @@ describe('toString', () => {
         expect(result).toBe(symbol.toString());
     });
 
-    it('should convert a number to a string', () => {
-        const result = toString(1);
-        expect(result).toBe('1');
-    });
+
 });
