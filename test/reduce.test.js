940a67ccb8c196d3ea90f3fbf33b9b7d93eb58df
Samantha Bavautdin <samantha.bavautdin@tuni.fi>
2023-11-25 16:42:18 +0200

    Unit tests for reduce.js according to test plan

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -2,8 +2,40 @@ import reduce from '../src/reduce.js';
 
 describe('reduce', () => {
     it('should reduce an array of numbers to their sum', () => {
-        const result = reduce([1, 2, 3, 4], (sum, n) => sum + n, 0);
-        expect(result).toBe(10);
+        const result = reduce([1.0, 2.0, 3.1, 6.1], (sum, n) => sum + n, 0);
+        expect(result).toBe(6.1);
+    });
+
+    it('should reduce an array with one number element to its sum that is its value', () => {
+        const result = reduce([1.0], (sum, n) => sum + n, 0);
+        expect(result).toBe(1.0);
+    });
+
+    it('should reduce an array of numbers to their sum plus initial accumulator', () => {
+        const result = reduce([1.0, 2.0, 3.1, 6.1], (sum, n) => sum + n, 4.0);
+        expect(result).toBe(10.1);
+    });
+
+    it('should reduce an array with one number element to its sum that is its value plus initial accumulator', () => {
+        const result = reduce([1.0], (sum, n) => sum + n, 1.1);
+        expect(result).toBe(2.1);
+    });
+
+    it('should throw an exception if iteratee is null', () => {
+        expect(() => reduce([1.0, 2.0, 3.0], null)).toThrow();
+    });
+
+    // Redundant?
+    it('should throw an exception if iteratee is null even for empty array collections', () => {
+        expect(() => reduce([], null)).toThrow();
+    });
+
+    it('should throw an exception if accumulator is not a number', () => {
+        expect(() => reduce([1.0, 2.0, 3.1], (sum, n) => sum + n, 'abc')).toThrow();
+    });
+
+    it('should throw an exception if an element in collection is not a number', () => {
+        expect(() => reduce([1.0, "abc", 3.1], (sum, n) => sum + n, 0)).toThrow();
     });
 
     it('should reduce an object to an array of keys grouped by their values', () => {
